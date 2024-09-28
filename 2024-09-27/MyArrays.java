46b88a6ca44a43728606e9e9d25175888dffb828
Seth04268 <sethfenton@Seth>
2024-09-27 22:17:31 -0400

    Second commit returnCopy Done

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -24,5 +24,40 @@ public class MyArrays {
 	}
 	return(returnString + "]");
 	}
+	public static int[] returnCopy(int[] ary) {
+		int[] returnArray = new int[ary.length]; 
+		for (int i = 0; i < ary.length; i++) {
+			 returnArray[i] = ary[i];
+		}
+		return(returnArray);
+	}
+
+
+	public static void main (String arg[]) {
+	int[] numberCount = {1,2,3,4,5,6,7,8};
+	int[] emptyForTest = new int[0]; 
+	//Printing the tests for the ints
+	System.out.println(convertArrayInt(numberCount));
+	System.out.println(convertArrayInt(emptyForTest));
+	
+	String[] arr = new String[]{"Apple","Banana","Orange"};
+	String[] arr2 = new String[0]; 
+	//Printing the tests for the ints
+	System.out.println(convertArrayString(arr));
+	System.out.println(convertArrayString(arr2));
+	
+	
+	int[] arr1 = {1,2,3,4,5,6,7,8};
+	System.out.println(convertArrayInt(arr1).equals(convertArrayInt(returnCopy(arr1))));
+	System.out.println(arr1 != returnCopy(arr1));
+	int[] arr4 = {1,2,3,4,5,6,7,8,9,10,11,12};
+	System.out.println(convertArrayInt(arr4).equals(convertArrayInt(returnCopy(arr4))));
+	System.out.println(arr4 != returnCopy(arr4));
+	int[] arr7 = new int[0];
+	System.out.println(convertArrayInt(arr7).equals(convertArrayInt(returnCopy(arr7))));
+	System.out.println(arr7 != returnCopy(arr7));
+	
+	
+	}
 
 }
