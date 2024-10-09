aa92d3b2026d74f5fa5c6a62071f6768338e92f0
Seth04268 <sethfenton@Seth>
2024-10-09 01:02:42 -0400

    most updated array methods, all complete, all tested

--- 2024-10-07-2DArrayMethods/ArrayDemo.java
+++ 2024-10-07-2DArrayMethods/ArrayDemo.java
@@ -1,14 +1,7 @@
 
-
 import java.util.Arrays;
 public class ArrayDemo{
-  public static void main(String[]args){
-    //write your tests here!
-    //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
-    //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
-    //do not use any other Arrays.method()
 
-  }
 
   //0. Include your prior methods to help you print a 1D/2D array of ints.
   public static String arrToString(int[]ary){
@@ -29,11 +22,14 @@ public class ArrayDemo{
   //as long as the parameters are different! (type and/or quantity must be different)
   //Pro tip: you should be using your 1D arrToString in this method!
   public static String arrToString(int[][]ary){
-  String returnString = "[";
-  for (int i = 0; i < ary.length; i++) {
-  	arrToString(ary[i]);
-  }	
-    return(returnString + "]");
+  if (ary.length == 0) {
+  return("[[]]");
+  }
+  String returnn = "[";
+  for(int i = 0; i < ary.length-1; i++){
+    returnn += arrToString(ary[i]) + ", ";
+  }
+  return (returnn + arrToString(ary[ary.length - 1]) + "]");
   }
 
   //1. Calculate and return how many elements equal zero in the 2D array.
@@ -44,7 +40,7 @@ public class ArrayDemo{
     }
     for (int i = 0; i < nums.length; i++) {
     	if (nums[i].length != 0) {
-    	for (int k = 0; k < nums[i].length; i++) {
+    	for (int k = 0; k < nums[i].length; k++) {
     		if (nums[i][k] == 0) {
     		 zeroCount += 1;
     		}
@@ -65,10 +61,8 @@ public class ArrayDemo{
     }
     for (int i = 0; i < nums.length; i++) {
     	if (nums[i].length != 0) {
-    	for (int k = 0; k < nums[i].length; i++) {
-    		if (nums[i][k] == 0) {
+    	for (int k = 0; k < nums[i].length; k++) {
     		 sum += nums[i][k];
-    		}
     	
     	}
       }
@@ -102,16 +96,45 @@ public static void replaceNegative(int[][] vals){
   //DO NOT use any built in methods that "copy" an array.
   //You SHOULD write a helper method for this.
   //If you don't see a good way to do that, you should stop and look at prior methods.
-  public static int[][] copy(int[][] nums){
-    return new int[1][1];
-  }
+    public static int[] copy(int[] ary) {
+		int[] returnArray = new int[ary.length];
+		for (int i = 0; i < ary.length; i++) {
+			 returnArray[i] = ary[i];
+		}
+		return(returnArray);
+}
+public static int[][] copy(int[][] nums){
+	int[][] realEmptyArray = new int[0][0];
+	int[] EmptyArray = new int[0];
+	if (nums.length == 0) {
+	return(realEmptyArray);
+	}
+	int[][] returnArray = new int[nums.length][nums[0].length];
+	for (int i = 0; i < nums.length; i++) {
+	if (nums[0].length != 0) {
+	returnArray[i] = copy(nums[i]);
+	}
+	else {
+	returnArray[i] = EmptyArray;
+	}
+	}
+	return(returnArray);
+	
+}
 
   //5. Rotate an array by returning a new array with the rows and columns swapped.
   //   You may assume the array is rectangular and neither rows nor cols is 0.
   //   e.g. swapRC({{1,2,3},{4,5,6}}) returns {{1,4},{2,5},{3,6}}
-  public static int[][] swapRC(int[][]nums){
-    return new int[1][1];
+public static int[][] swapRC(int[][]nums){
+int numsLength = nums[0].length;
+int[][] returnArray = new int[numsLength][nums.length];
+ for (int i = 0; i < nums.length; i++) {
+ 	for (int k = 0; k < nums[i].length; k++) {
+ 	returnArray[k][i] = nums[i][k];
+ 	}
   }
+  return returnArray;
+}
 
   //6. Make an HTML table by putting a table tag around the entire 2d array,
   //   tr tags around each row, and td tags around each value.
@@ -120,6 +143,69 @@ public static void replaceNegative(int[][] vals){
   //   e.g. htmlTable(new int[][]{{1,2},{3}})  returns:
   // "<table><tr><td>1</td><td>2</td></tr><tr><td>3</td></tr></table>"
   public static String htmlTable(int[][]nums){
-    return "";
+	String dataTable = "<table>";
+   for (int i = 0; i < nums.length; i++) {
+	   dataTable += "<tr>";
+ 	for (int k = 0; k < nums[i].length; k++) {
+		dataTable += "<td>" + nums[i][k] + "</td>";
+ 	}
+	dataTable += "</tr>";
+   }
+   dataTable += "</table>";
+   return(dataTable);
+  }
+   public static void main(String[]args){
+    //write your tests here!
+    //You can now use Arrays.toString(yourArray) instead of writing arrayToString again.
+    //Compare Arrays.toString(yourArray) to YOUR arrayToString() method to make sure yours is correct
+    //do not use any other Arrays.method()
+	  int[] testCase = {2,3,4,9};
+int[][] testCase1 = new int[][] {{2,3,4,9}, {2,3,4,9}, {2,3,4,9}};
+  int [][] testCaseVoid = new int[0][0];
+  int [][] testCase2 = {{1,2,3},{4,5,6}};
+    int [][] testCase8 = {{1,2,3},{},{4,5,6}};
+  int [][] testCaseVoidPartial = {{1,-2,3,4,-5,6,-7,-8},{},{-2,3,-4,5,-6,7,-8,9}};
+  int [][] testCase3 = {{1,-2,3,4,-5,6,-7,-8},{4,-5,6,-7,8,-9,10,11},{-2,3,-4,5,-6,7,-8,9}};
+System.out.println("arrToString test");
+System.out.println("\n");
+System.out.println(arrToString(testCase1));
+  System.out.println(arrToString(testCaseVoid));
+  System.out.println(arrToString(testCase3));
+  System.out.println("\n");
+ System.out.println("arr2DSum test");
+  System.out.println(arr2DSum(testCase1));
+  System.out.println(arr2DSum(testCase2));
+    System.out.println(arr2DSum(testCaseVoid));
+    System.out.println(arr2DSum(testCase8));
+  System.out.println(arr2DSum(testCase3));
+  System.out.println(arr2DSum(testCaseVoidPartial));
+  System.out.println("\n");
+  System.out.println("swapRC");
+  System.out.println(arrToString(swapRC(testCase2)));
+  System.out.println(arrToString(swapRC(testCase3)));
+  System.out.println("\n");
+  System.out.println("copy");
+  System.out.println(arrToString(copy(testCase2)));
+  System.out.println(arrToString(copy(testCaseVoidPartial)));
+  System.out.println(arrToString(copy(testCaseVoid)));
+  System.out.println(arrToString(copy(testCase3)));
+      System.out.println("\n");
+  System.out.println("HTML Table Function");
+   System.out.println(htmlTable(testCase2));
+  System.out.println(htmlTable(testCaseVoid));
+  System.out.println(htmlTable(testCaseVoidPartial));
+  System.out.println(htmlTable(testCase3));
+
+  replaceNegative(testCase2);
+  replaceNegative(testCaseVoid);
+  replaceNegative(testCaseVoidPartial);
+  replaceNegative(testCase3);
+  System.out.println("\n");
+System.out.println("replaceNegative");
+  System.out.println(arrToString(testCase2));
+  System.out.println(arrToString(testCaseVoid));
+  System.out.println(arrToString(testCaseVoidPartial));
+  System.out.println(arrToString(testCase3));
+
   }
 }
