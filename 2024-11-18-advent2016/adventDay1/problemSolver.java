50aee93d75652bae740bd4f695ceebe99a9eb0a5
Seth04268 <sfenton50@stuy.edu>
2024-11-19 11:46:04 -0500

    first commit for advent

---
+++ 2024-11-18-advent2016/adventDay1/problemSolver.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+public class problemSolver {
+  public static String[] parse(String filename) {
+    try {
+    File file = new File("input.txt");
+    Scanner parser = new Scanner(file);
+    String[] returnStringArray = parser.nextLine().split(", ");
+    return returnStringArray;
+
+  }
+  catch (FileNotFoundException ex) {
+    System.out.println("file not found");
+    String[] returnToMakeItCompile = new String[1];
+    return returnToMakeItCompile;
+  }
+  }
+  public static int solver(String[] data) {
+  int distanceUp = 0;
+  int distanceDown = 0;
+  int distanceLeft = 0;
+  int distanceRight = 0;
+  for (int i = 0; i < data.length; i++) {
+
+  }
+}
+  public static void main (String[] args) {
+    System.out.println(Arrays.toString(parse("input.txt")));
+  }
+}
