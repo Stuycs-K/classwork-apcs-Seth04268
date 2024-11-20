b1c704e6f7a15b7f62fb03f7b80be73d48110727
Seth04268 <sfenton50@stuy.edu>
2024-11-20 11:47:17 -0500

    final advent one solution (hopefully)

--- 2024-11-18-advent2016/adventDay1/problemSolver.java
+++ 2024-11-18-advent2016/adventDay1/problemSolver.java
@@ -22,10 +22,54 @@ public class problemSolver {
   int distanceRight = 0;
   for (int i = 0; i < data.length; i++) {
     String latestTurn = data[i].substring(0,1);
-    int latestDistance = data[i].substring(1);
+    int latestDistance = Integer.parseInt(data[i].substring(1));
+    String facing = "";
+    if (facing.equals("U")) {
+      if (latestTurn.equals("R")) {
+        facing = "R";
+        distanceRight += latestDistance;
+      }
+      else  {
+        facing = "L";
+        distanceLeft += latestDistance;
+      }
+    }
+    else if (facing.equals("R")) {
+      if (latestTurn.equals("R")) {
+        facing = "U";
+        distanceUp += latestDistance;
+      }
+      else  {
+        facing = "L";
+        distanceDown += latestDistance;
+      }
+    }
+    else if (facing.equals("D")) {
+      if (latestTurn.equals("R")) {
+        facing = "R";
+        distanceRight += latestDistance;
+      }
+      else {
+        facing = "L";
+        distanceLeft += latestDistance;
+      }
+    }
+    else {
+      if (latestTurn.equals("U")) {
+        facing = "L";
+        distanceRight += latestDistance;
+      }
+      else {
+        facing = "R";
+        distanceLeft += latestDistance;
+      }
+    }
   }
+  return Math.abs(distanceLeft - distanceRight) + Math.abs(distanceDown - distanceUp);
+
 }
   public static void main (String[] args) {
     System.out.println(Arrays.toString(parse("input.txt")));
+    System.out.println(solver(parse("input.txt")));
   }
 }
