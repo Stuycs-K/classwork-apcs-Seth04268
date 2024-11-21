49a5b74eea0c09a49ac8204ee9cee05d3e472041
Seth04268 <sfenton50@stuy.edu>
2024-11-21 11:05:31 -0500

    modifications for problem 2

--- 2024-11-18-advent2016/adventDay1/problemSolver.java
+++ 2024-11-18-advent2016/adventDay1/problemSolver.java
@@ -16,57 +16,83 @@ public class problemSolver {
   }
   }
   public static int solver(String[] data) {
-  int distanceUp = 0;
-  int distanceDown = 0;
-  int distanceLeft = 0;
-  int distanceRight = 0;
+int x = 0;
+int y = 0;
+String facing = "U";
+ArrayList<int[]> pastValues = new ArrayList<int[]>(1000);
+
   for (int i = 0; i < data.length; i++) {
     String latestTurn = data[i].substring(0,1);
     int latestDistance = Integer.parseInt(data[i].substring(1));
-    String facing = "";
+
     if (facing.equals("U")) {
       if (latestTurn.equals("R")) {
         facing = "R";
-        distanceRight += latestDistance;
+        x += latestDistance;
       }
       else  {
         facing = "L";
-        distanceLeft += latestDistance;
+        latestDistance *= -1;
+        x += latestDistance;
       }
     }
     else if (facing.equals("R")) {
       if (latestTurn.equals("R")) {
-        facing = "U";
-        distanceUp += latestDistance;
+        facing = "D";
+        latestDistance *= -1;
+        y += latestDistance;
       }
       else  {
-        facing = "L";
-        distanceDown += latestDistance;
+        facing = "U";
+        y += latestDistance;
       }
     }
     else if (facing.equals("D")) {
       if (latestTurn.equals("R")) {
-        facing = "R";
-        distanceRight += latestDistance;
-      }
-      else {
         facing = "L";
-        distanceLeft += latestDistance;
-      }
-    }
-    else {
-      if (latestTurn.equals("U")) {
-        facing = "L";
-        distanceRight += latestDistance;
+        latestDistance *= -1;
+        x += latestDistance;
       }
       else {
         facing = "R";
-        distanceLeft += latestDistance;
+        x += latestDistance;
       }
     }
-  }
-  return Math.abs(distanceLeft - distanceRight) + Math.abs(distanceDown - distanceUp);
+    else if (facing.equals("L")){
+      if (latestTurn.equals("R")) {
+        facing = "U";
+         y += latestDistance;
+      }
+      else  {
+        facing = "D";
+        latestDistance *= -1;
+        y += latestDistance;
+      }
+    }
 
+    for (int k = 0; k < pastValues.size(); k++) {
+      if (pastValues.get(k)[0] == x && pastValues.get(k)[1] == y) {
+        return Math.abs(x) + Math.abs(y);
+      }
+    }
+if (latestDistance > 0) {
+    for (int z = x - latestDistance; z < x; z++) {
+      for (int l = y - latestDistance; l < y; l++) {
+    pastValues.add(new int[] {x,y});
+  }
+}
+}
+if (latestDistance < 0) {
+for (int z = x; z > 1; z--) {
+  for (int l = 0; l > 1; l--) {
+pastValues.add(new int[] {x,y});
+}
+  }
+}
+
+
+  }
+  return(10000000);
 }
   public static void main (String[] args) {
     System.out.println(Arrays.toString(parse("input.txt")));
