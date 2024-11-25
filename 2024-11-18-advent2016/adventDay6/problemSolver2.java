63f5b753559adafd029542766c83acf17b91810b
Seth04268 <safenton08@Seth-PC.>
2024-11-24 23:58:24 -0500

    thank god it was super clonable and took like five minutes

---
+++ 2024-11-18-advent2016/adventDay6/problemSolver2.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+public class problemSolver2 {
+
+public static int minindexOfArray(int[] data) {
+int minValue = 10000000;
+for (int o = 0; o < data.length; o++) {
+if (minValue > data[o] && data[o] > 0) {
+minValue = data[o];
+}
+}
+for (int o = 0; o < data.length; o++) {
+if (minValue == data[o]) {
+return o;
+}
+}
+return 3;
+}
+
+  public static ArrayList<String[]> parse(String filename) {
+    try {
+    ArrayList<String[]> returnStringArray = new ArrayList<String[]>(0);
+    File file = new File(filename);
+    Scanner parser = new Scanner(file);
+while (parser.hasNextLine()) {
+    returnStringArray.add(parser.nextLine().split(""));
+    }
+    return returnStringArray;
+
+  }
+  catch (FileNotFoundException ex) {
+    System.out.println("file not found");
+    ArrayList<String[]> returnToMakeItCompile = new ArrayList<String[]>(0);
+    return returnToMakeItCompile;
+  }
+  }
+  
+  public static String solver(ArrayList<String[]> data) {
+ String returnString = "";
+  String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+ for (int k = 0; k < data.get(0).length; k++) {
+int[] alphabetCount = new int[26];
+for (int i = 0; i < data.size(); i++) {
+ for (int z = 0; z < 26; z++) {
+	if (data.get(i)[k].equals(alphabet[z])) {
+	alphabetCount[z] = alphabetCount[z] + 1;
+	}
+}
+}
+//System.out.println(Arrays.toString(alphabetCount));
+//System.out.println(alphabet[maxindexOfArray(alphabetCount)]);
+returnString += alphabet[minindexOfArray(alphabetCount)];
+
+}
+
+return returnString;
+}
+
+
+  public static void main (String[] args) {
+     int[] arrayTest = {0,0,0,0,0,0,5,0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,0};
+     //System.out.println(parse("input.txt"));
+System.out.println(solver(parse("input.txt")));
+System.out.println(minindexOfArray(arrayTest));
+
+  }
+}
